f1711e8a1bfd2ef9d20dcbc2b3fc4212aceee46e
Kyle Roebling <kroebling@outlook.com>
2020-03-14 13:03:30 -0500

    added d3 queue

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
+/*
+Geography 575 D3 Project
+Author: Kyle Roebling
+Date: 3/14/2020
+Description: This map looks at the South Carolina Democratic Primary (2020). It looks at the 
+percentage of vote for Biden,Buttigieg,Gabbard,Klobuchar,Sanders,Steyer and Warren along with the 
+demographics of race,age,income and education to discover insights on the profiles of candiates support
+base.
+*/
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+//begin script when window loads
+window.onload = setMap();
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    accessToken: 'pk.eyJ1Ijoia3JvZWJsaW5nIiwiYSI6ImNqeXczaGplMjB3YjgzYmxyZGU1OG90bXUifQ.ItIrq8YGHvZIilkcx-U8Ag'
-}).addTo(mymap);
+
+
+//set up choropleth map
+function setMap(){
+
+    //use queue to parallelize asynchronous data loading
+    d3.queue()
+        .defer(d3.csv, "data/SouthCarolina_Data.csv") //load attributes from csv
+        //.defer(d3.json, "data/SouthCarolina_Counties.topojson") //load choropleth spatial data
+        .await(callback);
+
+
+};
+
