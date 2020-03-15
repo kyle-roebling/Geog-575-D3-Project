7ae667761022de3ff6355f1fbbe8d2e1d8f374f6
Kyle Roebling <kroebling@outlook.com>
2020-03-15 17:00:37 -0500

    counties are mapped and projected

--- js/main.js
+++ js/main.js
@@ -15,16 +15,52 @@ window.onload = setMap();
 
 //set up choropleth map
 function setMap(){
-
+    
+    //map frame dimensions
+    var width = 960,
+        height = 460;
+    
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+    
+    //create Albers equal area conic projection centered on France
+    var projection = d3.geoAlbers()
+        .center([0, 33.83])
+        .rotate([81.16, 0, 0])
+        .parallels([43, 62])
+        .scale(5000)
+        .translate([width / 2, height / 2]);
+    
+    // create projection path generator
+    var path = d3.geoPath()
+        .projection(projection);
+    
+    
+    // Download csv dataset and json counties for South Carolina
     Promise.all([
           d3.csv("/data/SouthCarolina_Data.csv"),
           d3.json("/data/SouthCarolina_Counties.json")
-        ]).then(function(d) {
-            data = d[0];
-            counties = topojson.feature(d[1],d[1].objects.SouthCarolina_Counties).features;
-            console.log(data);
+        ]).then(function(data) {
+            dataset = data[0];
+            //Convert topojson into geojson format
+            counties = topojson.feature(data[1],data[1].objects.SouthCarolina_Counties).features;
+            console.log(dataset);
             console.log(counties);
-      
+        
+            //add South Carolina Counites to map
+            var regions = map.selectAll(".SouthCarolina_Counties")
+                .data(counties)
+                .enter()
+                .append("path")
+                .attr("class", function(d){
+                    return "counties " + d.properties.FIPS;
+                })
+                .attr("d", path);
+
         });
 
 
