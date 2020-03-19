43ff961bf1cbf706d2196c6af14869d01f85e2a9
Kyle Roebling <kroebling@outlook.com>
2020-03-18 19:40:10 -0500

    added colorscale for choropleth

--- js/main.js
+++ js/main.js
@@ -8,10 +8,17 @@ demographics of race,age,income and education to discover insights on the profil
 base.
 */
 
+//First line of main.js...wrap everything in a self-executing anonymous function to move to local scope
+(function(){
+
+//pseudo-global variables
+var attrArray = ["GEO_ID","NAME","Biden","Buttigieg","Gabbard","Klobuchar","Sanders","Steyer","Warren","White%","Black%","20_54%","55_85+%","UniversityDegree"];
+var expressedArray = ["Biden","Buttigieg","Gabbard","Klobuchar","Sanders","Steyer","Warren","White%","Black%","20_54%","55_85+%","UniversityDegree"];
+var expressed = expressedArray[0]; //initial attribute
+
+
 //begin script when window loads
 window.onload = setMap();
-
-
 
 //set up choropleth map
 function setMap(){
@@ -45,13 +52,33 @@ function setMap(){
           d3.csv("/data/SouthCarolina_Data.csv"),
           d3.json("/data/SouthCarolina_Counties.json")
         ]).then(function(data) {
+            
+            counties = joinData(data);
+        
+            //create the color scale
+            var colorScale = makeColorScale(data[0]);
+        
+            addCounties(counties,map,path,colorScale);
+    
+        
+        console.log(counties);
+
+        });
+
+    
+
+};// End of set map function
+   
+//This function joins the csv data to the GeoJson layer
+function joinData(data){
+    
             dataset = data[0];
             //Convert topojson into geojson format
             counties = topojson.feature(data[1],data[1].objects.SouthCarolina_Counties).features;
             
             //Join the csv file to the geojson file
             //variables for data join
-            var attrArray = ["GEO_ID","NAME","Biden","Buttigieg","Gabbard","Klobuchar","Sanders","Steyer","Warren","White%","Black%","20_54%","55_85+%","UniversityDegree"];
+
         
            
             //loop through csv to assign each set of csv attribute values to geojson region
@@ -83,22 +110,56 @@ function setMap(){
                 };
                             
             };
-        
-            
-            //add South Carolina Counites to map
-            var regions = map.selectAll(".SouthCarolina_Counties")
-                .data(counties)
-                .enter()
-                .append("path")
-                .attr("class", function(d){
-                    return "counties " + d.properties.FIPS;
-                })
-                .attr("d", path);
-        
-        console.log(counties);
-
-        });
-
-    
-
+    return counties;
 };
+    
+//Function that adds counties to the map
+function addCounties(counties,map,path,colorScale){
+    
+        //add South Carolina Counites to map
+        var regions = map.selectAll(".SouthCarolina_Counties")
+            .data(counties)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "counties " + d.properties.FIPS;
+            })
+            .attr("d", path)
+            .style("fill", function(d){
+            return colorScale(d.properties[expressed]);
+            });
+};
+    
+//Function to create the color scale for the choropleth
+
+function makeColorScale(data){
+    var colorClasses = [
+        "#D4B9DA",
+        "#C994C7",
+        "#DF65B0",
+        "#DD1C77",
+        "#980043"
+    ];
+    
+//create color scale generator
+var colorScale = d3.scaleQuantile()
+    .range(colorClasses);
+
+//build array of all values of the expressed attribute
+var domainArray = [];
+for (var i=0; i<data.length; i++){
+    var val = parseFloat(data[i][expressed]);
+    domainArray.push(val);
+};
+
+//assign array of expressed values as scale domain
+colorScale.domain(domainArray);
+
+return colorScale;
+};
+    
+    
+    
+    
+    
+})(); //last line of main.js
